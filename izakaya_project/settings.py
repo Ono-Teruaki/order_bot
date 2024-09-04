b861baa443309c7d09f5c2b2de434a5d2cd49be4
onoteruaki <terusan.o@icloud.com>
2024-09-04 14:33:45 +0900

    a

--- izakaya_project/settings.py
+++ izakaya_project/settings.py
@@ -131,7 +131,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # 以下を追加
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
